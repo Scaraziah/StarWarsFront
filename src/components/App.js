af4365746152675fb415911258823571eb8a2b75
Larry Hatcer <actswolf@gmail.com>
2022-01-27 17:18:41 -0600

    Added a form to add addtional episodes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,45 @@
 import React, { useState } from "react";
+import {nanoid} from 'nanoid'
 import data from "../mock-data.json"
+
 
 const App = () => {
   const [episodes, setEpisodes] = useState(data);
+  const [addFormData, setAddFormData] =useState({
+    name: '',
+    season: 0,
+    episodeNumber: 0,
+    location: '',
+    comment: ''
+  });
+
+  const handleAddFormChange = (event) => {
+    event.preventDefault();
+  
+    const fieldName = event.target.getAttribute('name');
+    const fieldValue = event.target.value;
+
+    const newFormData = {...addFormData};
+    newFormData[fieldName] = fieldValue
+
+    setAddFormData(newFormData)
+  }
+
+  const handleAddFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newEpisode = {
+      id: nanoid(),
+      name: addFormData.name,
+      season: addFormData.season,
+      episodeNumber: addFormData.episodeNumber,
+      location: addFormData.location,
+      comment: addFormData.comment
+    };
+
+    const newEpisodes = [...episodes, newEpisode];
+    setEpisodes(newEpisodes)
+  };
 
 return <div className="app-container">
   <table>
@@ -12,7 +49,7 @@ return <div className="app-container">
         <th>Season</th>
         <th>Episode</th>
         <th>Where to find</th>
-        <th>Notes</th>
+        <th>Comment</th>
       </tr>
     </thead>
     <tbody>
@@ -22,12 +59,52 @@ return <div className="app-container">
         <td>{episode.season}</td>
         <td>{episode.episodeNumber}</td>
         <td>{episode.location}</td>
-        <td>{episode.notes}</td>
+        <td>{episode.comment}</td>
       </tr>
       ))}
       
     </tbody>
   </table>
+
+  <h2>Add New Episodes</h2>
+  <form onSubmit={handleAddFormSubmit}>
+    <input
+      type = "text"
+      name="name"
+      required="required"
+      placeholder="Episode Name"
+      onChange={handleAddFormChange}
+    />
+    <input
+      type = "number"
+      name="season"
+      required="required"
+      placeholder="What Season"
+      onChange={handleAddFormChange}
+    />
+    <input
+      type = "number"
+      name="episodeNumber"
+      required="required"
+      placeholder="Episode Number"
+      onChange={handleAddFormChange}
+    />
+    <input
+      type = "text"
+      name="location"
+      required="required"
+      placeholder="Where can you find"
+      onChange={handleAddFormChange}
+    />
+    <input
+      type = "text"
+      name="comment"
+      required="required"
+      placeholder="Comment"
+      onChange={handleAddFormChange}
+    />
+    <button type="submit">Add Episode</button>
+  </form>
 </div>
 };
 
